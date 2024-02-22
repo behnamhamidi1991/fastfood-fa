4ca776e9a4578e647a38d8f00b7f04edd2e13fb9
behnamhamidi1991 <eslp91@hotmail.com>
2024-02-22 16:00:21 +0330

    Commit Blog

--- src/pages/Blog/BlogSinglePost/BlogSinglePost.jsx
+++ src/pages/Blog/BlogSinglePost/BlogSinglePost.jsx
@@ -9,7 +9,19 @@ const BlogSinglePost = () => {
 
   return (
     <div className="singleblog-page">
-      <div className="singleblog-image"></div>
+      <div className="singleblog-page-right">
+        <div className="singleblog-image"></div>
+        <div className="singleblog-content">
+          <h3>{singlePost.title}</h3>
+          <p>{singlePost.content}</p>
+        </div>
+        <div className="comments"></div>
+      </div>
+      <div className="singleblog-page-left">
+        <div className="singleblog-sidebar-top"></div>
+        <div className="singleblog-sidebar-middle"></div>
+        <div className="singleblog-sidebar-bottom"></div>
+      </div>
     </div>
   );
 };
